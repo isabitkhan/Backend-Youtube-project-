2024237648b24437723f9939a913cd77c9d7bf7c
Sabit Khan <mesabitkhan@gmail.com>
2025-04-17 12:56:31 +0500

    write utils code ApiError ApiResponse AsyncHandler

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import dotenv from "dotenv";
-import connectDB from "./db/index.js";
+import connectDB from "./db/index.db.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () =>
+      console.log(`Server is runing on port ${process.env.PORT || 8000}`)
+    );
+  })
+  .catch((error) => console.log(`Mongo DB connection Failed !! `, error));
